359f3f16a9477f445e7866ecb619f5693066d9af
rejaul <rejaulvoice@gmail.com>
2024-12-24 21:32:33 +0000

    I have connected the server with MongoDb

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,8 +8,45 @@ const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.omdcb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // created database
+    const db = client.db('eTutor')
+    const tutorialsCollection = db.collection('language')
+
+    // save tutorial data in db
+    app.post('/add-tutorial', async(req, res)=>{
+        
+    })
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res)=> {
