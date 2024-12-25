3fb41554ce59b74b78bb263089de21bfa7d229bf
rejaul <rejaulvoice@gmail.com>
2024-12-25 12:03:10 +0000

    Added a delete api to delete one tutorial

--- index.js
+++ index.js
@@ -73,6 +73,14 @@ async function run() {
         res.send(result)
     })
 
+    // delete single tutorial api
+    app.delete('/tutorial-delete/:id', async (req, res)=> {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await tutorialsCollection.deleteOne(query)
+      res.send(result)
+    })
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
